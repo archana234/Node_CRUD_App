a9bda23da78eb6558c03bd46ec4217d38e461a67
Durgam Archana <durgam.archana@qentelli.com>
2022-01-25 17:24:39 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// Importing expressJS
+const express = require('express')
+const app = express()
+
+// Inbuilt function of express server
+app.get('/',(req,res) =>{
+    res.send('Hello World!')
+
+})
+
+const PORT = 5000
+
+app.listen(PORT,() =>{
+    console.log(`server running at port ${PORT}`)
+})
