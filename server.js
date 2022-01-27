f05364ee8c30420b2859ff2e0dc62b0e41fd3636
Durgam Archana <durgam.archana@qentelli.com>
2022-01-27 18:08:51 +0530

    posting and fetching data done

--- server.js
+++ server.js
@@ -1,50 +1,117 @@
-// Importing expressJS
+// Importing expressjs
 
-// EXpress JS for creating server and api get,post,put and delete
+
+
+//express js for creating server and api to get,post,put and delete
+
 const express = require('express')
-// Body Parsers for getting the data through the urls
+
+
+
+//body Parser for getting the data through the urls
+
 const bodyParser = require('body-parser')
 
-//const {request} = require('express')
 
-// Importing Mongo client
+
+//Importing Mongo Client
+
 const MongoClient = require('mongodb').MongoClient
 
-// Const app controls the entire app with express functional constructor
+
+
+//const app controls the entire app with express finctional constructor
+
+//server and api's
+
 const app = express()
 
 
-// We are saying expressjs that to use body parser urlencoded to be true
-app.use(bodyParser.urlencoded({extended:true}))
+
+//wee are saying express.js that to use body parser url encoded to be true
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
-// Database Connection String
+//Db connection String
+
 const connectionString = "mongodb+srv://ARCHANA:Nikitha@cluster0.nie0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-// Connecting the database
-MongoClient.connect(connectionString,(err,client)=>{
-    if(err){
-        return console.error(err)
-    }
-    console.log('connected to the database')
+
+
+
+//connecting the db
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+
+    .then(client => {
+
+        console.log('connected to database-server')
+
+        const db = client.db('star-war-quotes')
+
+        const quotesCollection = db.collection('quotes')
+
+
+
+        //create route with creating the quote
+
+        app.post('/quotes', (req, res) => {
+
+            quotesCollection.insertOne(req.body)
+
+                .then(result => {
+
+                    res.send(result)
+
+                })
+
+                .catch(error => console.error(error))
+
+        })
+        // 2. Reading data from MongoDB
+        app.get('/getall', (req, res) => {
+            //Finding the collection quotes and changing object of object to array of objects
+            db.collection('quotes').find().toArray()
+                //waiting for the promise to send us the result back
+                .then(result => {
+                    res.send(result)
+                })
+                // Waiting for the promise to send us the error back
+                .catch(error => console.error(error))
+        })
+        // 3. Updating data from MongoDB
+        // app.put('/updatequote', (req,res)=>{
+        //    quotesCollection.findOneAndUpdate()
+        //   .then(result=>{
+        //      res.send(result)
+        //   })
+        //   .catch(error=>console.error(error))
+        //   })
+
+
+
+
+
+    }).catch(console.error)  // MongoDB Atlas Cluster/Server Connection Error
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile('index.html')
 
 })
 
-// Create Route with Creating the quote
-app.post('/quotes',(req,res)=>{
-    res.send(req.body)
-})
 
-
-// Inbuilt function of express server
-app.get('/',(req,res) =>{
-    //res.send("hello world!")
-    res.sendFile(_index.html)
-
-})
 
 const PORT = 3000
 
-app.listen(PORT,() =>{
+
+
+app.listen(PORT, () => {
+
     console.log(`server running at port ${PORT}`)
+
 })
